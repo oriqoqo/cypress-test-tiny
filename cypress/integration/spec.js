e55b41811b6e892b2fa9b44747c01e8fa8d5754b
Ori <ori.grundstein@iqoqo.co>
2019-03-17 00:42:37 +0200

    test does not fail

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,14 @@
-describe('page', () => {
-  it('works', () => {})
-})
+/// <reference types="Cypress" />
+
+describe('Basic test', async () => {
+  beforeEach(() => {
+    cy.visit("www.google.com");
+  })
+
+  it('Should fail', async () => {
+    cy.get('.main-button').should('be.visible');
+    cy.get('.main-button').click();
+    cy.get('.-modal-body').should('be.visible');
+    expect(1).to('have.text', 2);
+  });
+});
